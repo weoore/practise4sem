4a2064152334565b1f7804e8af1b4014c28da70e
Ilia <126543228+weoore@users.noreply.github.com>
2024-09-23 09:55:41 +0600

    Add files via upload

---
+++ Work.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Практикум по программированию')
+
+st.markdown("---")
+
+st.subheader('ФИО')
+st.markdown('Любаков Илья Александрович')
+
+st.subheader('Группа')
+st.markdown('МО-221')
+
